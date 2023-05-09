9ee01cac5f5c0ce17c19afab9100cbe7ed8e3001
commicat <commicat@commicatui-MacBookAir.local>
2023-05-09 14:31:05 +0900

    Feat: Add link to account pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 import LogoImg from "/logo.png";
 import { Link } from "react-router-dom";
+import LinkButton from "components/LinkButton";
 
 const Container = styled.div`
   margin-top: 4rem;
 `;
 
 const Logo = styled.img`
+  display: block;
   width: 40rem;
   object-fit: contain;
-  text-align: center;
+`;
+
+const StyledLinkButton = styled(LinkButton)`
+  margin-top: 1rem;
+  margin-left: 1rem;
+  width: 10rem;
 `;
 
 function HomePage() {
@@ -17,6 +24,12 @@ function HomePage() {
     <Container>
       <Link to="/">
         <Logo src={LogoImg} alt="Logo" />
+      </Link>
+      <Link to="/signin">
+        <StyledLinkButton>로그인</StyledLinkButton>
+      </Link>
+      <Link to="/signup">
+        <StyledLinkButton>회원가입</StyledLinkButton>
       </Link>
     </Container>
   );

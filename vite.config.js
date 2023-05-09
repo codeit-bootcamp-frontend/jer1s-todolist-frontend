7ebc834b939fd6a1badcf48c5d286908da13368e
commicat <commicat@commicatui-MacBookAir.local>
2023-05-09 13:17:57 +0900

    Chore: Edit vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      assets: path.resolve(__dirname, "src/assets"),
+      components: path.resolve(__dirname, "src/components"),
+      contexts: path.resolve(__dirname, "src/contexts"),
+      hooks: path.resolve(__dirname, "src/hooks"),
+      pages: path.resolve(__dirname, "src/pages"),
+      utils: path.resolve(__dirname, "src/utils"),
+    },
+  },
+});

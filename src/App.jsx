7e261df3cd83873c710852e2e163ff63fd269dfa
commicat <commicat@commicatui-MacBookAir.local>
2023-05-10 23:15:38 +0900

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import GlobalStyle from "components/GlobalStyle";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { CookiesProvider } from "react-cookie";
 import DefaultLayout from "components/DefaultLayout";
 import AccountLayout from "components/AccountLayout";
 import HomePage from "pages/HomePage";
@@ -7,26 +9,27 @@ import SigninPage from "pages/SigninPage";
 import SignupPage from "pages/SignupPage";
 import ForgotPasswordPage from "pages/ForgotPasswordPage";
 import NotFoundPage from "pages/NotFoundPage";
-import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<DefaultLayout />}>
-            <Route index element={<HomePage />} />
-          </Route>
-          <Route path="/" element={<AccountLayout />}>
-            <Route path="signin" element={<SigninPage />} />
-            <Route path="signup" element={<SignupPage />} />
-            <Route path="forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <CookiesProvider>
+      <HelmetProvider>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<DefaultLayout />}>
+              <Route index element={<HomePage />} />
+            </Route>
+            <Route path="/" element={<AccountLayout />}>
+              <Route path="signin" element={<SigninPage />} />
+              <Route path="signup" element={<SignupPage />} />
+              <Route path="forgot-password" element={<ForgotPasswordPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
+    </CookiesProvider>
   );
 }
 
